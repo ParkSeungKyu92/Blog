3e52a503898119d91ef7fed75320f1cb6441b2de
ParkSeungKyu <effortsk@naver.com>
2021-01-29 22:45:04 +0900

    사용할 Route 정리 기본 틀 추가 로그인, 회원가입, 글쓰기, 포스트, 포트스 목록

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import PostPageList from './pages/PostListPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Route component={PostPageList} path={['/@:username','/']} exact></Route>
+    <Route component={LoginPage} path="/login"></Route>
+    <Route component={RegisterPage} path="/register"></Route>
+    <Route component={WritePage} path="/write"></Route>
+    <Route component={PostPage} path="/@:username/:postId"></Route>
+    {/* /@:username은 localhost/@helloworld 같은 경로에서 helloworld를 username 파라미터로 읽을 수 있게 해준다. */}
+    </>
   );
 }
 
---
+++ src/pages/PostListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostPageList = () => {
+    return <div>포스트 목록</div>;
+};
+
+export default PostPageList;
