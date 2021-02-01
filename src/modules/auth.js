9a2b3f720ed6d038799add686771b15ecf2c3ac3
ParkSeungKyu <effortsk@naver.com>
2021-02-01 23:46:04 +0900

    auth Redux 수정  - changeField &  initailizeState 추가 authForm Container 생성  - auth redux를 이용한 컨테이너

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,13 +1,42 @@
 import { createAction, handleActions } from 'redux-actions';
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+import produce from 'immer';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITAILIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const initailState = {};
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({form, key, value}) => ({
+        form,  //register || login
+        key,   //username, password, passwordConfirm 
+        value  //current value
+    })
+);
+
+export const initailizeForm = createAction(INITAILIZE_FORM, form => form);
+
+const initailState = {
+    login : {
+        username : '',
+        password : ''
+    },
+    register : {
+        username : '',
+        password : '',
+        passwordConfirm : ''
+    }
+};
 
 const auth = handleActions(
     {
-        [SAMPLE_ACTION] : (state, payload) => state,
+        [CHANGE_FIELD] : (state, { payload : { form, key, value } }) => 
+            produce(state, draft => {
+                draft[form][key] = value;
+            }),
+        [INITAILIZE_FORM] : (state, { payload : form }) => ({
+            ...state,
+            [form] : initailState[form]
+        })
     },
     initailState
 );
