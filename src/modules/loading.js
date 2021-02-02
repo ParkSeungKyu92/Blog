903a21961157d0d4684631c6b0d2f025b6f116b4
ParkSeungKyu <effortsk@naver.com>
2021-02-02 22:14:32 +0900

    loading redux 생성 & rootReducer에 등록

---
+++ src/modules/loading.js
@@ -0,0 +1,31 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+    START_LOADING, requestType => requestType
+);
+
+export const finishLoading = createAction(
+    FINISH_LOADING, requestType => requestType
+);
+
+const initailState = {};
+
+const loading = handleActions(
+    {
+        [START_LOADING] : (state, action) => ({
+            ...state,
+            [action.payload] : true,
+        }),
+        [FINISH_LOADING] : (state, action) => ({
+            ...state,
+            [action.payload] : false,
+        }),
+    },
+    initailState
+);
+
+export default loading;
