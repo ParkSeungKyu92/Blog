36b003d6a0ced527feb6a28c275fcc998d3a9994
ParkSeungKyu <effortsk@naver.com>
2021-02-03 22:37:33 +0900

    Login 구현

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initailizeForm } from '../../modules/auth';
+import { changeField, initailizeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-
-const LoginForm = () => {
+const LoginForm = ( { history }) => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({auth}) => ({
-            form : auth.login
-        })
-    );
+    const { form, auth, authError, user } = useSelector(({auth, user}) => ({
+            form : auth.login,
+            auth : auth.auth,
+            authError : auth.authError,
+            user : user.user
+    }));
+    
 
     const onChange = e => {
         const { value, name } = e.target;
@@ -22,11 +26,30 @@ const LoginForm = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        const { username, password } = form;
+        dispatch(login({username, password}));
     }
 
     useEffect(() => {
         dispatch(initailizeForm('login'));
     }, [dispatch])
+
+    useEffect(() => {
+        if (auth) {
+            dispatch(check());
+        }
+        if (authError) {
+            console.log(authError);
+            return;
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if(user) {
+            //user Data success response
+            history.push('/');
+        }
+    }, [user, history]);
 
     return (
         <AuthForm
@@ -38,4 +61,4 @@ const LoginForm = () => {
     );
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
