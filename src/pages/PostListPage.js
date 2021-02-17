2b045c4f4af04f35acb6b7e28d8e0a5392d59d12
ParkSeungKyu <effortsk@naver.com>
2021-02-17 22:23:32 +0900

    PostViewer 컴포넌트 생성

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PostViewer from '../components/post/PostViewer';
 import HeaderContainer from '../containers/common/HeaderContainer';
 
 const PostPageList = () => {
     return (
         <div>
             <HeaderContainer/>
+            <PostViewer/>
         </div>
     );
 };
