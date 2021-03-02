46156a30677339a960ab86e1d04cad4297215e87
ParkSeungKyu <effortsk@naver.com>
2021-03-02 22:14:29 +0900

    title meta data 추가

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
 import RegisterPage from './pages/RegisterPage';
 import PostPageList from './pages/PostListPage';
+import { Helmet } from 'react-helmet-async';
 
 
 function App() {
   return (
     <>
+    <Helmet>
+      <title>박승규님 블로그~</title>
+    </Helmet>
     <Route component={PostPageList} path={['/@:username','/']} exact></Route>
     <Route component={LoginPage} path="/login"></Route>
     <Route component={RegisterPage} path="/register"></Route>
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer, { rootSaga } from './modules';
 import createSagaMiddleware from 'redux-saga';
 import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
@@ -31,7 +32,9 @@ loadUser();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
